6a4eaa04df11f81b250ac92a3ce337a079a8f6f4
maxmaruf222e <maxmaruf222@gmail.comm>
2022-10-29 20:45:34 +0600

    call a controller from Route

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        return 'This is the index() method inside the PostController';
+    }
 }
